b4fd05a60ae7aab54a31bc7054f8fb5eadf509ce
ZhengWei Guo <139207033+wayne-guo-super@users.noreply.github.com>
2025-03-26 03:46:52 -0400

    Complete basic functions
    Signed-off-by: ZhengWei Guo <139207033+wayne-guo-super@users.noreply.github.com>

--- src/components/3DScene/rotateFace.tsx
+++ src/components/3DScene/rotateFace.tsx
@@ -1,10 +1,9 @@
 import * as THREE from "three";
-//import { animated, useSpring } from "@react-spring/three";
 
-const rotateFace = (layer: any, axis: "x"|"y"|"z", direction: number) => {
+const rotateFace = (layer: THREE.Mesh[], axis: "x"|"y"|"z", direction: number) => {
     const angle = Math.PI / 2 * direction;
     const newColor = new THREE.Color(0xff0000); // Example color: red
-    layer.forEach(cube => {
+    layer.forEach(cube=> {
         if (axis === "x") {
             cube.rotation.x += angle;
         }

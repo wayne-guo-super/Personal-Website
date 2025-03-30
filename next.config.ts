8f94d9a45ef6000c4063b99a90a1bfbbef4632bb
ZhengWei Guo <139207033+wayne-guo-super@users.noreply.github.com>
2025-03-30 04:20:37 -0400

    R2 Test
    Signed-off-by: ZhengWei Guo <139207033+wayne-guo-super@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-    /*output: 'export',*/
+    output: 'export',
     images: { unoptimized: true },
     /* config options here */
     eslint: {

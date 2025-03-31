4226a6212268492dc2e37bc1e39a444849b86f09
ZhengWei Guo <139207033+wayne-guo-super@users.noreply.github.com>
2025-03-31 02:58:42 -0400

    R2 Test
    Signed-off-by: ZhengWei Guo <139207033+wayne-guo-super@users.noreply.github.com>

--- functions/source/[file].js
+++ functions/source/[file].js
@@ -1,8 +1,12 @@
 export async function onRequestGet(ctx) {
-    console.log("once again"); // 调试输出
+    console.log("API function triggered!"); // 观察是否触发
+    console.log("Request URL:", ctx.request.url);
+
     const path = new URL(ctx.request.url).pathname.replace("/source/", "");
+    console.log("Fetching R2 file:", path);
+
     const file = await ctx.env.source.get(path);
-    if (!file) return new Response(null, { status: 404 });
+    if (!file) return new Response(console.log("File not found in R2!"), { status: 404 });
     return new Response(file.body, {
         headers: { "Content-Type": file.httpMetadata.contentType },
     });

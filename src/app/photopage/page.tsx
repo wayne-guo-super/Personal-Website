bf57c0bb2240da787342e28f9322d187eb4d98d8
ZhengWei Guo <139207033+wayne-guo-super@users.noreply.github.com>
2025-12-19 15:18:10 -0500

    fix: resolve R2 private images returning 404 in Next.js Image
    Signed-off-by: ZhengWei Guo <139207033+wayne-guo-super@users.noreply.github.com>

--- src/app/photopage/page.tsx
+++ src/app/photopage/page.tsx
@@ -9,13 +9,13 @@ export default function PhotoPage() {
                 -/-
                 <div className="flex flex-col">
                     <div>
-                        <Image
+                        <img
                             src="/source/booth/DSC08501.jpg"
                             width={800}
                             height={600}
                             alt="Montreal"
                             className="shadow-lg"
-                        ></Image>
+                        ></img>
                     </div>
 
                     <div className="flex items-center md:items-start flex-col">
@@ -28,13 +28,13 @@ export default function PhotoPage() {
                 -/-
                 <div className="flex flex-col">
                     <div>
-                        <Image
+                        <img
                             src="/source/booth/DSC08832.jpg"
                             width={800}
                             height={600}
                             alt="Yangshuo"
                             className="shadow-lg"
-                        ></Image>
+                        ></img>
                     </div>
 
                     <div className="flex items-center md:items-start flex-col">
@@ -57,13 +57,13 @@ export default function PhotoPage() {
                 </p>
                 <div className="flex flex-col">
                     <div>
-                        <Image
+                        <img
                             src="/source/booth/DSC08867.jpg"
                             width={800}
                             height={600}
                             alt="Yangshuo"
                             className="shadow-lg"
-                        ></Image>
+                        ></img>
                     </div>
 
                     <div className="flex items-center md:items-start flex-col">
